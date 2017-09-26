6ee7263b3e32deeee20195da8a718d557dedc05f
gustavoballeste <gustavoballeste@gmail.com>
2017-09-26 19:00:51 -0300

    fix: Captura dos campos corretor para inclusão

---
+++ 25-bares-e-restaurantes-pet-friendly-footer.php
@@ -0,0 +1,6 @@
+<?php
+
+ include("include/conectar-footer.php");
+
+ header("location: 25-bares-e-restaurantes-pet-friendly-download.html");
+ ?>
---
+++ como-economizar-tempo-e-dinheiro-com-seu-automovel-footer.php
@@ -0,0 +1,6 @@
+<?php
+
+ include("include/conectar-footer.php");
+
+ header("location: como-economizar-tempo-e-dinheiro-com-seu-automovel-download.html");
+ ?>
